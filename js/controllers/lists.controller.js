754d1b48f56804050236ccc41e4cdc3f3342f6e9
aemrich <adam.emrich@gmail.com>
2017-01-25 15:26:15 -0500

    done

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,3 +1,53 @@
 'use strict';
 // Lists Controller
 
+class ListsController {
+  constructor() {
+    this.$addListForm = $('#add_list')
+    this.$listTitleInput = $('#list_title')
+    this.$selectListMenu = $('#select_list')
+    this.$addTaskForm = $('#add_task')
+    this.$wrapper = $('#wrapper')
+  }
+
+  init() {
+    var thisClass = this
+    this.$addTaskForm.hide()
+
+    this.$addListForm.on("submit", function onSubmit(evt) {
+      evt.preventDefault()
+      thisClass.$addTaskForm.show()
+
+      appendList(this, thisClass)
+    })
+
+    function appendList(thisList, thisClass) {
+      let title = thisClass.$listTitleInput.val()
+
+      let list = new List(title)
+      var $listsSection = $(thisList).parent().children('#lists')
+
+      $listsSection.append(list.listEl())
+
+      thisClass.$selectListMenu.append(`<option value="${list.id}" >${title}</option>`)
+
+      var $destroyButton = $(thisList).parent().find('button.destroy-list')
+      $destroyButton.on("click", function onDestroy(evt){
+        evt.preventDefault()
+        debugger
+        // thisClass.$selectListMenu.children(`option`).attr(`value=${list.id}`).remove()
+
+        // for(var i = 0; i < selector.length; i++){
+        //     var temp = selector[i].getAttribute('value')
+        // 	if( parseInt(temp) === list.id){
+        //        console.log('true')
+        // 	} else {
+        //     	console.log('false')
+        // 	}
+        // }
+
+        $(this).parent().parent().remove()
+      })
+    }
+  }
+}
--- js/controllers/tasks.controller.js
+++ js/controllers/tasks.controller.js
@@ -1,3 +1,32 @@
 'use strict';
 // Tasks Controller
+class TasksController{
+  constructor(){
+    this.$addTaskForm = $('#add_task')
+    this.$taskDescriptionInput = $('#task_description')
+    this.$selectListMenu = $('#select_list')
+    this.$taskPriorityInput = $('#task_priority')
+    this.$wrapper = $('#wrapper')
+  }
 
+  init(){
+    var thisClass = this
+    this.$addTaskForm.on('submit', function(evt) {
+      evt.preventDefault()
+      let desc = thisClass.$taskDescriptionInput.val()
+      let priority = thisClass.$taskPriorityInput.val()
+      let selector = parseInt(thisClass.$selectListMenu.val(), 10)
+
+      let task = new Task(desc, priority, List.all[selector])
+      $(document).find(`#list-${selector}`).append(`<li data-id="${task.id}"><button class="destroy-task">x</button>${desc}, ${priority}</li>`)
+
+      var $taskDestroyButton = $(this).parent().find('button.destroy-task')
+      $taskDestroyButton.on("click", function onDestroy(evt){
+        evt.preventDefault()
+        $(this).parent().remove()
+        List.all[selector]['tasks'][task.id] = null
+      })
+    })
+  }
+
+}
